1d6d9a49179433039671b6c8929db1d3c90128f9
Harsh Gahlot <gahlotharsh19@gmail.com>
2025-11-09 16:53:56 +0530

    second commit

--- Jarvis.py
+++ Jarvis.py
@@ -1,11 +1,15 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
+import os
+import cv2
+
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[0].id)
-engine.setProperty('voices',voices[0].id)
+engine.setProperty('voice',voices[0].id)
 
 #To convert text to speech
 def speak(audio):
@@ -19,7 +23,7 @@ def takecommand():
   with sr.Microphone() as source:
     print("Listening...")
     r.pause_threshold = 1
-    audio = r.listen(source, timeout=1,phrase_time_limit=5)
+    audio = r.listen(source, timeout=5,phrase_time_limit=10)
   
   try:
     print("Recognizing...")
@@ -35,8 +39,39 @@ def takecommand():
 def wish():
   hour =int(datetime.datetime.now().hour)
 
-  
+  if hour >= 0 and hour <=12:
+    speak("Good Morning")
+  elif hour > 12 and hour < 18:
+    speak("Good Afternoon")
+  else:
+    speak("Good Evening")
+  speak("I am Jarvis sir, please tell me how can I help you")
 
 if __name__ == "__main__":
-  takecommand()
-  # speak()
+  wish()
+  # while True:
+  if 1:
+
+    query = takecommand().lower()
+
+    # logic building for tasks
+
+    if "open notepad" in query:
+      npath = "C:\\Windows\\notepad.exe"
+      os.startfile(npath)
+
+    elif "open command prompt" in query:
+      os.system("start cmd")
+
+    elif "open camera" in query:
+      cap = cv2.VideoCapture(0)
+      while True:
+        ret, img = cap.read()
+        cv2.imshow('webcam',img)
+        k = cv2.waitKey(50)
+        if k == 27:
+          break
+      cap.release()
+      cv2.destroyAllWindows()
+
+    
